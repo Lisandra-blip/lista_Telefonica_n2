7e38018b331425bff821a29dab944ca6a15fd88b
Lucas Batista <lucas.dbatista@b2wdigital.com>
2022-05-28 20:34:01 -0300

    Add singleton

--- app/src/main/java/br/univali/contacts/ContactDatabase.java
+++ app/src/main/java/br/univali/contacts/ContactDatabase.java
@@ -1,9 +1,22 @@
 package br.univali.contacts;
 
+import android.content.Context;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Contact.class}, version = 1)
 public abstract class ContactDatabase extends RoomDatabase {
+    private static ContactDatabase instance;
+
+    public static ContactDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context, ContactDatabase.class, "contact-database").build();
+            return instance;
+        } else {
+            return instance;
+        }
+    }
+
     public abstract ContactDao contactDao();
 }
