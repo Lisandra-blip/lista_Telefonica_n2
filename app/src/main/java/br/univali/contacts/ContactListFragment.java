faef0b47e9cf40eba22653d126bac8a8e6170770
André Fragas <andrefragas14@gmail.com>
2022-05-28 19:59:27 -0300

    Continuando

--- app/src/main/java/br/univali/contacts/ContactListFragment.java
+++ app/src/main/java/br/univali/contacts/ContactListFragment.java
@@ -19,9 +19,6 @@ public class ContactListFragment extends Fragment {
         FragmentContactListBinding binding = FragmentContactListBinding.inflate(inflater, container, false);
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         List<Contact> contacts = new ArrayList<>();
-        contacts.add(new Contact("Marcos Kohler", "(47) 98447-8012"));
-        contacts.add(new Contact("André Fragas", "(47) 98447-8013"));
-        contacts.add(new Contact("João Bohmer", "(47) 98447-8014"));
         binding.recyclerView.setAdapter(new ContactListAdapter(contacts));
         return binding.getRoot();
     }
