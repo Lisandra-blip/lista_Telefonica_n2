971af69a79028c66058ea55496e91eb17a21d95e
Jgcbitj <67125125+JoaoBoemer@users.noreply.github.com>
2022-05-28 20:10:13 -0300

    Criando classe DAO

---
+++ app/src/main/java/br/univali/contacts/ContactDao.java
@@ -0,0 +1,11 @@
+package br.univali.contacts;
+
+import androidx.room.Query;
+
+import java.util.List;
+
+public interface ContactDao {
+    @Query("SELECT * FROM Contact")
+    List<Contact> findAll();
+
+}
