2129ed19eed64f7a431a51e892e2ccf0776c4b53
Lucas Batista <lucas.dbatista@b2wdigital.com>
2022-05-28 20:47:35 -0300

    Add annotation

--- app/src/main/java/br/univali/contacts/ContactDao.java
+++ app/src/main/java/br/univali/contacts/ContactDao.java
@@ -1,9 +1,11 @@
 package br.univali.contacts;
 
+import androidx.room.Dao;
 import androidx.room.Query;
 
 import java.util.List;
 
+@Dao
 public interface ContactDao {
     @Query("SELECT * FROM Contact")
     List<Contact> findAll();
