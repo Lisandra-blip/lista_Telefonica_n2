46043eb0429dc64d1c35708fb1a6a52b773b7cbd
Lisandra <lisandragazaniga@gmail.com>
2022-05-30 23:37:07 -0300

    adicionei spinner coluna

--- app/src/main/java/br/univali/contacts/Contact.java
+++ app/src/main/java/br/univali/contacts/Contact.java
@@ -17,15 +17,18 @@ public class Contact implements Serializable {
     @ColumnInfo
     private String phone;
 
+    @ColumnInfo
+    private String type;
 
     public Contact() {
 
     }
 
-    public Contact(int id, String name, String phone) {
+    public Contact(int id, String name, String phone, String type) {
         this.id = id;
         this.name = name;
         this.phone = phone;
+        this.type = type;
     }
 
     public int getId() {
@@ -50,6 +53,14 @@ public class Contact implements Serializable {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public static class Comparator implements java.util.Comparator<Contact> {
